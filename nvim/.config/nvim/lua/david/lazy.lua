34be7d5ded98b2a6a427ad05386aba221278df77
jellyfin <dumb@email.com>
2023-11-09 21:58:10 +0100

    Change fuzzy alg

--- nvim/.config/nvim/lua/david/lazy.lua
+++ nvim/.config/nvim/lua/david/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
-    "nvim-telescope/telescope-fzy-native.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
     
     -- Treeshitter
     {
