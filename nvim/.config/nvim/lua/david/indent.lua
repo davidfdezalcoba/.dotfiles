651b8dae952d37dcd1de44b8b74ba522d99009eb
David Fernandez Alcoba <david.fernandez1@ibm.com>
2023-11-09 14:04:17 +0100

    Refactor indent-blankline

--- nvim/.config/nvim/lua/david/indent.lua
+++ nvim/.config/nvim/lua/david/indent.lua
@@ -1,6 +1,3 @@
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
-    use_treesitter = true
+require("ibl").setup{
+    indent = { char = "▏" },
 }
